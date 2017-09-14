c17fbcf9b05b054c0336e609db6ce80da1c8f943
Punith <puneethu@gmail.com>
2017-09-14 18:19:22 +0530

    Initial commit - skeleton of project

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var webpack = require("webpack");
+var path = require('path');
+
+module.exports = {
+
+	entry: {
+		app: './src/app.js'
+	},
+	output: {
+		filename: 'build/bundle.js',
+        sourceMapFilename: 'build/bundle.map'
+	},
+	devtool: '#source-map',	
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader',
+				query:{
+					presets:['react', 'es2015']
+				}
+			}
+		]
+	},
+	plugins: [		  
+		new webpack.DefinePlugin({
+			// A common mistake is not stringifying the "production" string.
+			'process.env.NODE_ENV': JSON.stringify('production')
+		  }),
+		  new webpack.optimize.UglifyJsPlugin({
+			compress: {
+			  warnings: false
+			}
+		  }) ,
+	
+	  ]
+}
